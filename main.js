d7c4e578c985c6578e67a01640b8bf31d8a25293
grigoryan.m <waldm4rk@gmail.com>
2023-09-01 14:56:40 +0400

    finished project

--- main.js
+++ main.js
@@ -10,9 +10,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const randColor = document.getElementsByClassName("background")[0];
     const guessedColor = document.getElementsByClassName("background")[1];
 
-    const randRed = getRandomInt(256);
-    const randGreen = getRandomInt(256);
-    const randBlue = getRandomInt(256);
+    let randRed = getRandomInt(256);
+    let randGreen = getRandomInt(256);
+    let randBlue = getRandomInt(256);
+
+    const button = document.getElementsByClassName("submit")[0];
 
     randColor.style.backgroundColor =  'rgb(' + randRed + ', ' + randGreen + ', ' + randBlue + ')';
 
@@ -24,5 +26,27 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     blue.addEventListener("input", () => {
         guessedColor.style.backgroundColor =  'rgb(' + red.value + ', ' + green.value + ', ' + blue.value + ')';
+    });
+
+    button.addEventListener("click", () => {
+        let redPerc = 100 - ((Math.abs(red.value - randRed) / 255) * 100); // Red accuracy %;
+        let bluePerc = 100 - ((Math.abs(blue.value - randBlue) / 255) * 100);
+        let greenPerc = 100 - ((Math.abs(green.value - randGreen) / 255) * 100);
+    
+        let accuracy = Math.ceil((redPerc + greenPerc + bluePerc) / 3);
+
+
+        alert("Your guess was: (" + red.value.toString() + ", " + green.value.toString() + ", " + blue.value.toString() + ")\n"
+        + "Secret color was: (" + randRed.toString() + ", " + randGreen.toString() + ", " + randBlue.toString() + ")\n"
+        + "Your accuracy is " + accuracy.toString() + "%"
+        );
+        randRed = getRandomInt(256);
+        randGreen = getRandomInt(256);
+        randBlue = getRandomInt(256);
+
+        randColor.style.backgroundColor =  'rgb(' + randRed + ', ' + randGreen + ', ' + randBlue + ')';
+        red.value = 127;
+        green.value = 127;
+        blue.value = 127;
     });
 });
